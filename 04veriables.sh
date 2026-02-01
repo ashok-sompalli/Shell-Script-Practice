e13c5f3baf7863968b6fe27bd016fd32d7e1449a
ashok <smashok88@gmail.com>
2026-02-01 16:27:51 -0500

    veriables-v1 commit

--- 04veriables.sh
+++ 04veriables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=Suresh
-person2=Ramesh
+person1=Ashok
+person2=Souresh
 echo "$person1:: Hello $person2, How are you?"
 echo "$person2:: Hi $person1, I am fine thanks for asking. How are you doing?"
 echo "$person1:: doing fine. What are you learning $person2?"
