e70b90e2e96b1511cf57002266c390a485a65261
ashok <smashok88@gmail.com>
2026-02-01 17:22:06 -0500

    veriables-timestamp commit

--- 06veriables.sh
+++ 06veriables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-START_Time=$(date %s)
+START_Time=$(date +%s)
 
 echo "Script start at $START_Time"
 
 sleep 10
 
-END_Time=$(date %s)
+END_Time=$(date +%s)
 
 Total_Time=(($START_Time - $END_Time))
 
